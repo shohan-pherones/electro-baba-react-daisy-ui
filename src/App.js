b6c799fa4f564a3aad7c48cd4394dd83cb98f9fe
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-13 01:02:32 +0600

    daisy added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div>
-      <h1 className="text-5xl font-semibold text-red-500">Hello, react!</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
